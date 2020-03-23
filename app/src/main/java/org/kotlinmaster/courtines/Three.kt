4d70ea83c5667133c32818c72a49d2578135e32c
fangxiang <xiaof14792@gmail.com>
2020-03-23 10:02:13 +0800

    Kotlin官方文档「协程基础」

---
+++ app/src/main/java/org/kotlinmaster/courtines/Three.kt
@@ -0,0 +1,15 @@
+package org.kotlinmaster.courtines
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking { //this: CoroutineScope
+    launch { //launch a new coroutine in the scope of runBlocking
+        delay(1000L)
+        println("World!")
+    }
+
+    println("Hello,")
+}
---
+++ app/src/main/java/org/kotlinmaster/courtines/Two.kt
@@ -0,0 +1,17 @@
+package org.kotlinmaster.courtines
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit> {
+    val job = GlobalScope.launch { //launch a coroutine and keep a reference to its Job
+        delay(1000L)
+        println("World.")
+    }
+
+    println("Hello,")
+//    delay(2000L)
+    job.join() //wait until child coroutine completes
+}
